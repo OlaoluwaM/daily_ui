407be2e53cb688e14c71aa1c06548e7ada03a876
Olaoluwa Mustapha <jomeemustapha@gmail.com>
2021-04-04 10:34:58 -0500

    Initialize project using Create React App

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import themeObj from '../context/theme';
+
+import { Suspense } from 'react';
+import { ThemeProvider } from 'styled-components';
+import { LazyMotion, domAnimation } from 'framer-motion';
+import { Route, Switch, useLocation } from 'react-router-dom';
+
+export default function App() {
+  const location = useLocation();
+
+  return (
+    <ThemeProvider theme={themeObj}>
+      <LazyMotion features={domAnimation}>
+
+        <>
+          <Suspense fallback={<p>Loading</p>}>
+            <Switch location={location} key={location.pathname}>
+              <Route exact path="/">
+                <div>Hello World</div>
+              </Route>
+            </Switch>
+          </Suspense>
+        </>
+
+      </LazyMotion>
+    </ThemeProvider>
+  );
+}
